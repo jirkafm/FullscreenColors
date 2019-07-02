ceebaf2db504fd0697eaa164646a126a4a75499c
jkaplan <jiri.kaplan@quest.com>
2019-07-02 11:38:16 +0200

    Terminate application by CTRL+Q

--- src/com/github/jirkafm/FullscreenColorsControl.java
+++ src/com/github/jirkafm/FullscreenColorsControl.java
@@ -5,10 +5,9 @@ import java.util.Random;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
-class FullscreenColorsControl implements Listener {
+class FullscreenColorsControl {
 
 	private final Random random;
 	private final Shell shell;
@@ -19,16 +18,31 @@ class FullscreenColorsControl implements Listener {
 	}
 
 	public void init() {
-		shell.getDisplay().addFilter(SWT.KeyDown, this);
+		shell.getDisplay().addFilter(SWT.KeyDown, this::generateColor);
+		shell.getDisplay().addFilter(SWT.KeyDown, this::exitSequenceCheck);
 	}
 
-	@Override
-	public void handleEvent(Event event) {
+	public void generateColor(Event event) {
 		final int keyCode = event.keyCode;
 		final int red = keyCode % 255;
 		final int green = random.nextInt(255);
 		final int blue = random.nextInt(255);
 		shell.setBackground(new Color(shell.getDisplay(), red, green, blue));
+	}
+
+	private void exitSequenceCheck(Event event) {
+		if (isExitKeySequence(event)) {
+			shell.close();
+		}
+	}
+
+	private boolean isExitKeySequence(Event event) {
+		final int keyCode = event.keyCode;
+		return isCtrlHold(event) && 'q' == keyCode;
+	}
+
+	private boolean isCtrlHold(Event event) {
+		return SWT.CTRL == (event.stateMask & SWT.CTRL);
 	}
 
 }
